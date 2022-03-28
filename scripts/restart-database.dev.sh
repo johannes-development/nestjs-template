344b046c00b86e2757ec9efd9ee574b5ec5a7695
Johannes Holzhauer <johannes.dev+github@protonmail.com>
2022-03-28 15:40:47 +0200

    updated script to use right platform

--- scripts/restart-database.dev.sh
+++ scripts/restart-database.dev.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-if [[ $platform == 'linux' ]]; then
-docker-compose -f docker-compose.dev.yml rm -f -s -v dev-database &&
-docker-compose -f docker-compose.dev.yml up -d dev-database &&
+echo "Restarting the development database..."
+
+echo "$OSTYPE"
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    docker-compose -f docker-compose.dev.yml rm -f -s -v dev-database &&
+    docker-compose -f docker-compose.dev.yml up -d dev-database
 else
-docker compose -f docker-compose.dev.yml rm -f -s -v dev-database &&
-docker compose -f docker-compose.dev.yml up -d dev-database &&
+    docker compose -f docker-compose.dev.yml rm -f -s -v dev-database &&
+    docker compose -f docker-compose.dev.yml up -d dev-database
 fi
 sleep 3 &&
 yarn prisma:migrate:dev
